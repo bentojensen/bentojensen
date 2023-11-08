67828881af6e507aa17b725b697365ab841072ec
BestBoj <bentojensen@users.noreply.github.com>
2023-11-08 03:59:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,141 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      
+<head>
+    <link rel="shortcut icon" href="../grafik/bent.o.jensen.person.dk.ico" />
+    <meta http-equiv="Content-Type" content="text/html; charset=us-ascii" />
+    <title>Bents Visitkort</title>
+    <link rel="stylesheet" type="text/css" href="global.css" />
+    <link rel="stylesheet" type="text/css" href="menuknap.css" />
+    <style type="text/css">
+        /*<![CDATA[*/
+        li
+        {
+            font-family: Verdana;
+            font-size: x-small;
+            font-weight: normal;
+            text-decoration: none;
+            color: #0000FF;
+            list-style-type: none;
+            line-height: 17px;
+            text-align: left;
+        }
+        /*]]>*/
+    </style>
+</head>
+<link rel="shortcut icon" href="../grafik/favicon.ico" />
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+<frameset rows="*" framespacing="0" frameborder="no" border="0">
+<frameset cols="110,*" framespacing="0" frameborder="no" border=
+  "0">
+<frame name="menu" src="http://webmaster.x10.bz/menu.htm" scrolling="no" marginwidth=
+    "3" marginheight="8" />
+<frame name="main" src="http://webmaster.x10.bz/start.htm" />
+
+<noframes>
+<div itemscope itemtype="http://data-vocabulary.org/Person">
+   Mit navn er <span itemprop="name">Bent O. jensen</span>. 
+  
+   Her er min hjemmeside: 
+   
+   <a href="http://bent.o.jensen.person.dk" itemprop="url">Bent Visitkort</a>.
+  
+   Jeg bor i 
+   <span itemprop="address" itemscope
+      itemtype="http://data-vocabulary.org/Address">
+      <span itemprop="locality">København</span>
+      <span itemprop="region">Danmark</span> 
+   
+   </span>
+.   
+</div>
+<body>
+<!-- BEGIN PERSON-BANNER 1.4 -->
+
+<center>
+<table>
+<tr>
+<td valign="top"><a href=
+              "http://pi.dk/cgi-bin/ref.cgi?id=person.dk" target=
+              "_top"><img src="http://www.pi.dk/pi60.gif" border=
+              "0" width="60" height="60" alt="Pi" /></a></td>
+
+<td>
+<center>
+<a href=
+                  "http://pi.dk/cgi-bin/ref.cgi?id=bannerperson.dk"
+                  target="_blank"><img src=
+                  "http://www.person.dk/banner/www.dit-navn.dk.gif"
+                  alt="Klik for at bes&#248;ge vores annonc&#248;r"
+                  border="0" width="468" height="60" /></a>
+</center>
+</td>
+
+<td valign="center"><a href=
+              "http://pi.dk/cgi-bin/ref.cgi?id=person.dk" target=
+              "_top"><img src="http://www.pi.dk/pi60.gif" border=
+              "0" width="60" height="60" alt="Pi" /></a></td>
+</tr>
+</table>
+</center><!-- END PERSON-BANNER 1.4 -->
+<script src="http://www.google-analytics.com/urchin.js"
+        type="text/javascript">
+</script><script type="text/javascript">
+//<![CDATA[
+             _uacct = "UA-341890-1";
+             urchinTracker();
+        //]]>
+        </script>
+
+
+</body>
+</noframes>
+</frameset>
+
+<a target="_top" href="index.htm" ><strong>My web 1.0</strong></a>
+<a href="start.htm" target="main"><img src="http://webmaster.x10.bz/grafik/bent5.jpg" border="0" width="80" height="113" alt="Til start - Bent O Jensen""></a>
+<a title="email" href="mailto:bent.o.jensen@person.dk">Bent.O.Jensen</a>
+<a title="Visitkort med tlfnr. og email" href="http://webmaster.x10.bz/start.htm" target="main">Visitkort</a>
+<a target="_blank" title="mobilsite" a href="http://webmaster.x10.bz/mobile/www/" rel="publisher">Mobilsite</a>
+<a target="_blank" title="Google plus" a href="https://plus.google.com/117418148894137950568/about/p/pub" rel="publisher">Google+</a>
+<a title="Mine bogmærker (mest IT)" href="http://protopage.com/bestboj?" target="main">Links</a>
+<a title="Mine bogmærker" href="http://webmaster.x10.bz/links/index.html" target="main">Links</a>
+<a title="Oplysninger om 2450 København SV version 3" href="http://sites.google.com/site/sydhavnenslinks/" target="main">Sydhavnen</a>
+<a title="Min Levnedsbeskrivelse" href="https://docs.google.com/document/pub?id=1H2EYFqN2ImIalEzQy9-_9Jl2Tx6QxLKtCwlsyO2vwnc" target="main">CV</a>
+
+<a title="Viser i eget vindue - bedre til udskrift" href="https://docs.google.com/document/pub?id=1H2EYFqN2ImIalEzQy9-_9Jl2Tx6QxLKtCwlsyO2vwnc" target="_blank">
+    udskriv CV</a>
+<a title="Min profil på LinkedIn" href="http://dk.linkedin.com/in/bentojensen" target="_blank">LinkedIn</a>
+<a title="nyheder.danmark.nu - RSS-feeds fra danske medier" href="http://protopage.com/nyheder.danmark.nu" target="main">Nyheder RSS</a>
+<a target="main" title="Kalender Danske mærkedage" href="http://webmaster.x10.bz/kalender/" >Kalender</a>
+<a target="_parent" title="Google menu" href="http://webmaster.x10.bz/mymenu/">Google</a>
+<a title="web 2.0: online services" target="_blank" href="http://webmaster.x10.bz/web20/">My Web 2.0</a>
+<a title="Siden er beregnet til mobiltlf." href="http://webmaster.x10.bz/mobil/" target="main">Mobiltlf. links</a> 
+<a title="Mobiltlf links" href="http://www.protopage.com/mobil.dk" target="main">mobil.dk</a>
+<a target="_top" href="index.htm">
+<img alt="" src="http://webmaster.x10.bz/grafik/Heraldik_edb_02.gif" width="98" height="100" class="style1">
+
+</frameset>
+<!-- Default Statcounter code for http://2450.cf https://
+webmaster.x10.bz/ -->
+<script type="text/javascript">
+var sc_project=5424274; 
+var sc_invisible=0; 
+var sc_security="d4a8c992"; 
+var scJsHost = "https://";
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/5424274/0/d4a8c992/0/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
+</html>
 
       <Footer />
     </div>
